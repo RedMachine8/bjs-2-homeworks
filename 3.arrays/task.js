f05e98c13f12a83946064f632f8f715092a74045
Vlad Sergeev <redmachine8@yandex.ru>
2025-02-28 08:55:58 -0800

    Выполненное задание 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (!users || !gender) return 0;
+    const filteredUsers = users.filter(user => user.gender === gender);
+
+    if (filteredUsers.length === 0) return 0;
+
+    const averageAge = filteredUsers.reduce((acc, user) => acc + user.age, 0) / filteredUsers.length;
+
+    return averageAge;
 }
