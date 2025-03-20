6bf71497a812f4e7f5cdac13b4a21613cb25e144
Vlad Sergeev <redmachine8@yandex.ru>
2025-03-20 05:34:25 -0700

    Выполненное задание 7

---
+++ 7.async/task.js
@@ -0,0 +1,62 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        if(!time || !callback) {
+            throw new Error("Отсутствуют обязательные элементы");
+        }
+
+        const existingClock = this.alarmCollection.find(clock => clock.time === time);
+        if(existingClock) {
+            console.warn("Уже присутствует звонок на это же время");
+        }
+
+        this.alarmCollection.push({callback, time, canCall: true});
+    }
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(clock => clock.time !== time);
+    }
+
+    getCurrentFormattedTime() {
+        const currentDate = new Date();
+        const hours = currentDate.getHours().toString().padStart(2, '0');
+        const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+        
+        const result = `${hours}:${minutes}`;
+        return result;
+    }
+
+    start() {
+        if(this.intervalId) {
+            return;
+        }
+
+        this.intervalId = setInterval(() => {
+            const currentTime = this.getCurrentFormattedTime();
+            this.alarmCollection.forEach(clock => {
+                if(clock.time === currentTime && clock.canCal) {
+                    clock.canCall = false;
+                    clock.callback();
+                }
+            });
+        }, 1000);
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(clock => clock.canCall = true);
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
